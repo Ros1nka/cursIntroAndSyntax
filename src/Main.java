6107a75f8207a99760ef7660412dd38a09da918f
Евгений Страхов <goodwin.s@mail.ru>
2024-08-05 18:51:05 +0300

    very hard

--- src/Main.java
+++ src/Main.java
@@ -2,68 +2,114 @@
 public class Main {
     public static void main(String[] args) {
 
-        Employee[] employees = new Employee[10];
+        EmployeeBook employeeBook = new EmployeeBook();
 
-        employees[0] = new Employee("Машков", "Владимир", "Львович", 1, 89);
-        employees[1] = new Employee("Хабенский", "Константин", "Юрьевич", 3, 87);
-        employees[2] = new Employee("Яковлева", "Елена", "Алексеевна", 4, 71);
-        employees[3] = new Employee("Петренко", "Игорь", "Петрович", 2, 75);
-        employees[4] = new Employee("Агуреева", "Полина", "Владимировна", 2, 66);
-        employees[5] = new Employee("Безруков", "Сергей", "Витальевич", 1, 94);
-        employees[6] = new Employee("Боярская", "Елизавета", "Михайловна", 4, 73);
-        employees[7] = new Employee("Меньшиков", "Олег", "Евгеньевич", 5, 87);
-        employees[8] = new Employee("Ходченкова", "Светлана", "Викторовна", 5, 75);
-        employees[9] = new Employee("Пересильд", "Юлия", "Сергеевна", 2, 91);
+        System.out.println(employeeBook.addEmployee(new Employee("Машков", "Владимир", "Львович", 3, 89)));
+        System.out.println(employeeBook.addEmployee(new Employee("Хабенский", "Константин", "Юрьевич", 3, 87)));
+        System.out.println(employeeBook.addEmployee(new Employee("Яковлева", "Елена", "Алексеевна", 4, 71)));
+        System.out.println(employeeBook.addEmployee(new Employee("Петренко", "Игорь", "Петрович", 2, 75)));
+        System.out.println(employeeBook.addEmployee(new Employee("Агуреева", "Полина", "Владимировна", 2, 66)));
+        System.out.println(employeeBook.addEmployee(new Employee("Безруков", "Сергей", "Витальевич", 3, 94)));
+        System.out.println(employeeBook.addEmployee(new Employee("Боярская", "Елизавета", "Михайловна", 4, 73)));
+        System.out.println(employeeBook.addEmployee(new Employee("Меньшиков", "Олег", "Евгеньевич", 5, 87)));
+        System.out.println(employeeBook.addEmployee(new Employee("Ходченкова", "Светлана", "Викторовна", 5, 75)));
+        System.out.println(employeeBook.addEmployee(new Employee("Пересильд", "Юлия", "Сергеевна", 2, 91)));
 
-
-        Utils.viewAll(employees);
+        //удаляем сотрудника по id
+        int idForDelete = 3;
+        System.out.println(employeeBook.deleteEmployee(idForDelete) ? "Сотрудник удалён" : "Нет сотрудника с таким id");
         System.out.println();
 
-        System.out.println("Сумма затрат на ЗП в месяц = " + Utils.monthSum(employees));
-        System.out.println();
-
-        System.out.println("Сотрудник с минимальной ЗП: " + employees[Utils.minSalaryEmployee(employees)].getIdAndName());
-        System.out.println();
-
-        System.out.println("Сотрудник с максимальной ЗП: " + employees[Utils.maxSalaryEmployee(employees)].getIdAndName());
-        System.out.println();
-
-        System.out.println("Среднее значение зарплат = " + Utils.averageSalary(employees));
-        System.out.println();
-
-        for (Employee employee : employees) {
-            System.out.println(employee.getLastName() + " " + employee.getFirstName() + " " + employee.getPatronymic());
+        //получаем сотрудника по id
+        int idForGet = 10;
+        Employee employee = employeeBook.getEmployee(idForGet);
+        if (employee != null) {
+            System.out.println(employee.toString());
+        } else {
+            System.out.println("Нет сотрудника с таким id");
         }
         System.out.println();
 
-        //Повышенная сложность
+        //Получить список всех сотрудников
+        employeeBook.viewAll();
+
+        //Посчитать сумму затрат на ЗП в месяц
+        System.out.println("Сумма затрат на ЗП в месяц = " + employeeBook.monthSum());
+        System.out.println();
+
+        //Найти сотрудника с минимальной ЗП;
+        employee = employeeBook.minSalaryEmployee();
+
+        if (employee != null) {
+            System.out.println("Сотрудник с минимальной ЗП: " + employee.getIdAndName());
+        } else {
+            System.out.println("В книге нет сотрудников");
+        }
+        System.out.println();
+
+        //Найти сотрудника с максимальной ЗП;
+        employee = employeeBook.maxSalaryEmployee();
+
+        if (employee != null) {
+            System.out.println("Сотрудник с максимальной ЗП: " + employee.getIdAndName());
+        } else {
+            System.out.println("В книге нет сотрудников");
+        }
+        System.out.println();
+
+        //Среднее значение зарплат
+        System.out.println("Среднее значение зарплат = " + employeeBook.averageSalary());
+        System.out.println();
+
+        //Распечатать ФИО всех сотрудников
+        employeeBook.printFullName();
+        System.out.println();
+
+        //Проиндексировать зарплату
         int indexingPercentage = 10;
-        Utils.indexTheSalary(employees, indexingPercentage);
+        employeeBook.indexTheSalary(indexingPercentage);
 
+        //Сотрудник с минимальной зп в отделе
         int departmentNumber = 2;
-        System.out.println("Сотрудник с минимальной ЗП в отделе №" + departmentNumber +
-                ": " + employees[Utils.minSalaryDepartment(employees, departmentNumber)].getIdAndName());
+        employee = employeeBook.minSalaryDepartment(departmentNumber);
+
+        if (employee != null) {
+            System.out.println("Сотрудник с минимальной ЗП в отделе №" + departmentNumber + ": " + employee.getIdAndName());
+        } else {
+            System.out.println("В отделе нет сотрудников");
+        }
         System.out.println();
 
-        System.out.println("Сотрудник с максимальной ЗП в отделе №" + departmentNumber +
-                ": " + employees[Utils.maxSalaryDepartment(employees, departmentNumber)].getIdAndName());
+        //Сотрудник с максималоьной зп в отделе
+        employee = employeeBook.maxSalaryDepartment(departmentNumber);
+
+        if (employee != null) {
+            System.out.println("Сотрудник с максималоьной ЗП в отделе №" + departmentNumber + ": " + employee.getIdAndName());
+        } else {
+            System.out.println("В отделе нет сотрудников");
+        }
         System.out.println();
 
+        //Сумма затрат на ЗП по отделу
         System.out.println("Сумма затрат на ЗП по отделу №" + departmentNumber +
-                ": " + Utils.sumSalaryDepartment(employees, departmentNumber));
+                ": " + employeeBook.sumSalaryDepartment(departmentNumber));
         System.out.println();
 
+        //Средняя ЗП по отделу
         System.out.println("Средняя ЗП по отделу №" + departmentNumber +
-                ": " + Utils.averageSalaryDepartment(employees, departmentNumber));
+                ": " + employeeBook.averageSalaryDepartment(departmentNumber));
         System.out.println();
 
+        //Проиндексировать зарплату всех сотрудников отдела на процент
         int indexingPercentageDepartment = 15;
-        Utils.indexTheSalaryDepartment(employees, departmentNumber, indexingPercentageDepartment);
+        employeeBook.indexTheSalaryDepartment(departmentNumber, indexingPercentageDepartment);
 
-        Utils.printEmployeesDeportment(employees, departmentNumber);
+        //Напечатать всех сотрудников отдела
+        employeeBook.printEmployeesDeportment(departmentNumber);
 
-        int parameter = 80;
-        Utils.printSalaryIsLess(employees, parameter);
-        Utils.printSalaryIsMore(employees, parameter);
+        //Ввести: всех сотрудников с зп меньше числа; всех сотрудников с зп больше (или равно) числа
+        int parameter = 90;
+        employeeBook.printSalaryIsLess(parameter);
+        employeeBook.printSalaryIsMore(parameter);
     }
 }
